363ba6425fe3579cc2923ef5ba58e72a9e9ed905
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-14 18:22:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  {:railroads => {:rent_in_dollars => {:1 => ,:2 => ,:3 => ,:4 => }}}
+  {:railroads => {:rent_in_dollars => {:1 => 1,:2 => 1,:3 => 1,:4 => 1}}}
 end
 
 def monopoly_with_fourth_tier
